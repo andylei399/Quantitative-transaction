623be704a4cda9d266df3351cd6fffba4585b976
andylei399 <399394127@qq.com>
2022-02-12 18:20:23 +0800

    low prem : the first version

---
+++ low_prem/main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 12:15:31 2021
+
+@author: 39939
+"""
+
+from jisilu_lib import jisilu_lib
+from qmt_basic_lib import qmt_basic_lib
+from qmt_lib import qmt_lib
+
+lookup_csv = 'lookup.csv' ###
+bond_nums=3 ###
+threshold=5####
+prem_coef=0  ##
+price_coef=1  ###
+record_file='pos.csv'  ####
+stratgy_name='A'  ####
+max_capital = 1000000  ##
+Info='leizhijie'  ###
+accID='006354'   #####
+Mode= 0  ### 0: prem/dlow , 1:increase_diff 
+turn= 1  ## 1 : 1h , 2: 2h , 24:1day turn, it can used outside .....
+recordfile='record.csv'
+accountfile='account.csv'
+
+u0_csv = jisilu_lib(lookup_csv)
+        
+u0 = qmt_lib(bond_nums,threshold,prem_coef,price_coef,record_file,stratgy_name,max_capital,Info,accID,Mode,lookup_csv,recordfile,accountfile  )
+u0.run()
+
+     
